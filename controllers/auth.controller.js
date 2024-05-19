c8f635342aec00c5012ab0fec635f791cd6355d0
furkankaratekin <furkankaratekin.2k1@gmail.com>
2024-05-19 23:52:32 +0300

    auth update

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -3,11 +3,12 @@ import bcryptjs from "bcryptjs";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 dotenv.config();
-
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashedPassword = bcryptjs.hashSync(password, 10);
-  const newUser = new User({ username, email, password: hashedPassword });
+  const newUser = new User({ username, email, password: hashedPassword },process.env.JWT_SECRET);
+  
+  console.log(process.env.JWT_SECRET)
   try {
     await newUser.save();
     res.status(201).json({ message: "User created successfully" });
