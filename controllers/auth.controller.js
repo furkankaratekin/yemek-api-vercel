b6316dca79714d7bab80f93e78bd18966afdeca4
furkankaratekin <furkankaratekin.2k1@gmail.com>
2024-05-19 17:54:16 +0300

    usersignup update

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -7,13 +7,22 @@ dotenv.config();
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
+
+  // Şifreyi hash'le
   const hashedPassword = bcryptjs.hashSync(password, 10);
-  const newUser = new User({ username, email, password: hashedPassword });
+
+  // Yeni kullanıcı oluştur
+  const newUser = new User({
+    username,
+    email,
+    password: hashedPassword,
+  });
+
   try {
     await newUser.save();
     res.status(201).json({ message: "User created successfully" });
   } catch (error) {
-    next(error);
+    next(errorHandler(error)); // Hataları özel bir hata işleyiciye yönlendir
   }
 };
 
