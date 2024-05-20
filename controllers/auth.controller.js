be1b7ae687fa847c0d366903d0cc58e5eabff43b
furkankaratekin <furkankaratekin.2k1@gmail.com>
2024-05-20 10:22:17 +0300

    asdasd

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,14 +1,14 @@
 import User from "../models/user.model.js";
-import bcryptjs from "bcryptjs";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
+import bcrypt from 'bcrypt';
 dotenv.config();
 
 
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
-  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const hashedPassword = bcrypt.hashSync(password, 10);
   const newUser = new User({ username, email, password: hashedPassword });
   
   
@@ -25,7 +25,7 @@ export const signin = async (req, res, next) => {
   try {
     const validUser = await User.findOne({ email });
     if (!validUser) return next(errorHandler(404, "User not found"));
-    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    const validPassword = bcrypt.compareSync(password, validUser.password);
     if (!validPassword) return next(errorHandler(401, "Wrong credentials"));
 
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET, {
@@ -63,7 +63,7 @@ export const google = async (req, res, next) => {
       const generatedPassword =
         Math.random().toString(36).slice(-8) +
         Math.random().toString(36).slice(-8);
-      const hashedPassword = bcryptjs.hashSync(generatedPassword, 10);
+      const hashedPassword = bcrypt.hashSync(generatedPassword, 10);
       const newUser = new User({
         username:
           req.body.name.split(" ").join("").toLowerCase() +
@@ -99,7 +99,7 @@ export const signinId = async (req, res, next) => {
     const validUser = await User.findOne({ email });
     if (!validUser) return next(errorHandler(404, "User not found"));
 
-    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    const validPassword = bcrypt.compareSync(password, validUser.password);
     if (!validPassword) return next(errorHandler(401, "Wrong credentials"));
 
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET, {
