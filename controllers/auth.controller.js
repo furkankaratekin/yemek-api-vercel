2191f109b3d66d7f468c010134c93f0a52facdb8
furkankaratekin <furkankaratekin.2k1@gmail.com>
2024-05-20 10:31:58 +0300

    ASDADS

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -8,6 +8,8 @@ dotenv.config();
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
+  console.log(username);
+  console.log(password)
   const hashedPassword = await bcrypt.hash(password, 10);
   const newUser = new User({ username, email, password: hashedPassword });
   
